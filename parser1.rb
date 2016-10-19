cde78671b3c42f0fee25e790d91a7ca1825876ac
GMed14 <JorgeMedrano1996@yahoo.com>
2016-10-18 22:06:54 -0500

    Total Requests Made
    Code for Total Requests made

--- parser1.rb
+++ parser1.rb
@@ -2,3 +2,11 @@
 puts "Start"
 File.read("http_access_log")
 puts "End"
+
+    #How many total requests were made in the time period represented in the log?
+puts "================="
+puts "Total Number of Requests:"
+
+file=File.open("http_access_log","r")
+puts file.readlines.size
+puts "================="
